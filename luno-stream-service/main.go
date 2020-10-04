8a8f65a958e5993af921b45078fcab56045eb03f
Brendan Bosman <bhbosman@gmail.com>
2020-10-04 20:13:50 +0200

    Fix startup error

--- luno-stream-service/main.go
+++ luno-stream-service/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	serviceInternal "github.com/bhbosman/goLuno/luno-stream-service/internal"
 	"github.com/kardianos/service"
 	"log"
@@ -11,40 +10,36 @@ import (
 func main() {
 	svcFlag := flag.String("service", "", "Control the system service.")
 	flag.Parse()
+
+	program := serviceInternal.NewProgram()
+
+	svcConfig := &service.Config{
+		Name:        "bhbosman.Luno.Stream.Service",
+		DisplayName: "bhbosman.Luno.Stream.Service",
+		Description: "bhbosman.Luno.Stream.Service",
+		Option:      make(service.KeyValue),
+	}
+	svcConfig.Option["UserService"] = true
+	svcConfig.Option["KeepAlive"] = true
+	svcConfig.Option["RunAtLoad"] = true
+	svcConfig.Option["SessionCreate"] = true
+	svcConfig.Option["LaunchdConfig"] = ""
+
+	s, err := service.New(program, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	if len(*svcFlag) != 0 {
-
-		program := serviceInternal.NewProgram()
-
-		svcConfig := &service.Config{
-			Name:        "bhbosman.Luno.Stream.Service",
-			DisplayName: "bhbosman.Luno.Stream.Service",
-			Description: "bhbosman.Luno.Stream.Service",
-			Option:      make(service.KeyValue),
-		}
-		svcConfig.Option["UserService"] = true
-		svcConfig.Option["KeepAlive"] = true
-		svcConfig.Option["RunAtLoad"] = true
-		svcConfig.Option["SessionCreate"] = true
-		svcConfig.Option["LaunchdConfig"] = ""
-
-		s, err := service.New(program, svcConfig)
+		err := service.Control(s, *svcFlag)
 		if err != nil {
+			log.Printf("Valid actions: %q\n", service.ControlAction)
 			log.Fatal(err)
 		}
-
-		if len(*svcFlag) != 0 {
-			err := service.Control(s, *svcFlag)
-			if err != nil {
-				log.Printf("Valid actions: %q\n", service.ControlAction)
-				log.Fatal(err)
-			}
-			return
-		}
-		err = s.Run()
-		if err != nil {
-			return
-		}
-	} else {
-		log.Fatal(fmt.Errorf("service param not set"))
+		return
+	}
+	err = s.Run()
+	if err != nil {
+		return
 	}
 }
