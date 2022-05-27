53f80c6f7e3537091affecb071aab4f948de744f
Brendan Bosman <bhbosman@gmail.com>
2022-05-27 22:13:37 +0200

    refactor

--- luno-stream/main.go
+++ luno-stream/main.go
@@ -7,14 +7,14 @@ import (
 
 func main() {
 	lunoStreamApp, err := lunoStream.App(false,
-		//lunoStream.HttpListenerUrl("http://127.0.0.1:8080"),
-		//lunoStream.TextListenerUrl("tcp4://127.0.0.1:3000"),
-		//lunoStream.CompressedListenerUrl("tcp4://127.0.0.1:3001"),
-		//lunoStream.AddCurrencyPair(common.NewPairInformation("XBTZAR")),
-		//lunoStream.AddCurrencyPair(common.NewPairInformation("XBTEUR")),
-		//lunoStream.AddCurrencyPair(common.NewPairInformation("XBTUGX")),
-		//lunoStream.AddCurrencyPair(common.NewPairInformation("XBTZMW")),
-		//lunoStream.AddCurrencyPair(common.NewPairInformation("ETHXBT")),
+		lunoStream.HttpListenerUrl("http://127.0.0.1:8080"),
+		lunoStream.TextListenerUrl("tcp4://127.0.0.1:3000"),
+		lunoStream.CompressedListenerUrl("tcp4://127.0.0.1:3001"),
+		lunoStream.AddCurrencyPair(common.NewPairInformation("XBTZAR")),
+		lunoStream.AddCurrencyPair(common.NewPairInformation("XBTEUR")),
+		lunoStream.AddCurrencyPair(common.NewPairInformation("XBTUGX")),
+		lunoStream.AddCurrencyPair(common.NewPairInformation("XBTZMW")),
+		lunoStream.AddCurrencyPair(common.NewPairInformation("ETHXBT")),
 		lunoStream.AddCurrencyPair(common.NewPairInformation("BCHXBT")),
 	)
 	if err != nil {
